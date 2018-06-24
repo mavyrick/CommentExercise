ae292a9de2f6828cc812f7a0c1989bdc2d959126
Josh Sorokin <jrsorokin@gmail.com>
2018-06-25 02:10:28 +0300

    add read me

--- frontend/src/CommentContainer.js
+++ frontend/src/CommentContainer.js
@@ -3,50 +3,46 @@ import PropTypes from 'prop-types';
 import Filter from './Filter.js';
 import Comment from './Comment.js';
 
-  class CommentContainer extends Component {
+class CommentContainer extends Component {
 
-   constructor(props) {
-    super();
-    this.state = { 
-      filterData: ''
-    };
+ constructor(props) {
+  super();
+  this.state = { 
+    filterData: ''
+  };
+}
+
+myCallback = (dataFromFilter) => {
+  this.setState({ filterData: dataFromFilter });
+}
+
+render() {
+
+ const commentMap = this.props.data.map(comment => {
+  if (comment.email === this.state.filterData || this.state.filterData === '') {
+    return (
+      <Comment 
+      email={comment.email}
+      message={comment.message}
+      key={comment._id}
+      id={comment._id}
+      timestamp={comment.updatedAt}
+      handleDeleteComment={this.props.handleDeleteComment}
+      >
+      { comment.message }
+      </Comment> 
+      )
   }
-
-  myCallback = (dataFromFilter) => {
-    this.setState({ filterData: dataFromFilter });
-  }
-
-  render() {
-
-   const commentMap = this.props.data.map(comment => {
-    if (comment.email === this.state.filterData || this.state.filterData === '') {
-      return (
-        <Comment 
-        email={comment.email}
-        message={comment.message}
-        key={comment._id}
-        id={comment._id}
-        timestamp={comment.updatedAt}
-        handleDeleteComment={this.props.handleDeleteComment}
-        >
-        { comment.message }
-        </Comment> 
-        )
-    }
-  })
-  // if (this.props.data.size > 0) {
-   return (
-    <div className="container-wrapper">
-    <Filter handleFilter={this.myCallback} />
-    <div>
-    { commentMap }
-    </div>
-    </div>
-    )
- }
- // else {
-//   return (<div>No comments yet!</div>)
-// }
+})
+ return (
+  <div className="container-wrapper">
+  <Filter handleFilter={this.myCallback} />
+  <div>
+  { commentMap }
+  </div>
+  </div>
+  )
+}
 }
 
 CommentContainer.propTypes = {
